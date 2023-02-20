6303bc50bbfaa43d25c6e336e1fb64c8ab092d77
Toma <tomaandrei98@gmail.com>
2023-02-20 12:21:52 +0200

    Add Dynamic Arrays

---
+++ src/main/java/collections/implementation/array/DynamicArray.java
@@ -0,0 +1,103 @@
+package collections.implementation.array;
+
+public class DynamicArray {
+    int size;
+    int capacity = 10;
+    Object[] array;
+
+    public DynamicArray() {
+        this.array = new Object[capacity];
+    }
+
+    public DynamicArray(int capacity) {
+        this.capacity = capacity;
+        this.array = new Object[capacity];
+    }
+
+    public void add(Object data) {
+        if (size >= capacity) {
+            grow();
+        }
+
+        array[size] = data;
+        size++;
+    }
+
+    public void insert(int index, Object data) {
+        if (size >= capacity) {
+            grow();
+        }
+        for (int i = size; i > index; i--) {
+            array[i] = array[i - 1];
+        }
+        array[index] = data;
+        size++;
+    }
+
+    public void delete(Object data) {
+        for (int i = 0; i < size; i++) {
+            if (array[i] == data) {
+                for (int j = 0; j < (size - i - 1); j++) {
+                    array[i + j] = array[i + j + 1];
+                }
+                array[size - 1] = null;
+                size--;
+
+                if (size <= (capacity / 3)) {
+                    shrink();
+                }
+                break;
+            }
+        }
+    }
+
+    public int search(Object data) {
+        for (int i = 0; i < size; i++) {
+            if (array[i] == data) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    private void grow() {
+        int newCapacity = capacity * 2;
+        Object[] newArray = new Object[newCapacity];
+        for (int i = 0; i < size; i++) {
+            newArray[i] = array[i];
+        }
+        capacity = newCapacity;
+        array = newArray;
+    }
+
+    private void shrink() {
+        int newCapacity = capacity / 2;
+        Object[] newArray = new Object[newCapacity];
+        for (int i = 0; i < size; i++) {
+            newArray[i] = array[i];
+        }
+        capacity = newCapacity;
+        array = newArray;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder string = new StringBuilder();
+
+        for (int i = 0; i < capacity; i++) {
+            string.append(array[i]).append(", ");
+        }
+
+        if (string.length() > 0) {
+            string = new StringBuilder("[" + string.substring(0, string.length() - 2) + "]");
+        } else {
+            string = new StringBuilder("[]");
+        }
+
+        return string.toString();
+    }
+}
